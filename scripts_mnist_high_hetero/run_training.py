340abebf023268a34f2e8ef126df882d61e3af92
Luoxiaogan <luoxiaogan@mail.com>
2025-03-29 14:17:03 +0000

    add

--- scripts_mnist_high_hetero/run_training.py
+++ scripts_mnist_high_hetero/run_training.py
@@ -20,24 +20,83 @@ from utils import ring1, show_row , ring3, ring2, ring4
 from network_utils import get_matrixs_from_exp_graph
 
 
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# n=16
+# A,B = ring1(n=n)
+# k = 1
+# A = np.linalg.matrix_power(A, k)
+# show_row(A)
+# print(A.shape)
+
+# train_high_hetero(
+#     algorithm="PullDiag_GT",
+#     lr=2e-3,
+#     A=A,
+#     B=B,# 实际没用用到
+#     dataset_name="MNIST",
+#     batch_size=128,
+#     num_epochs=400,
+#     remark=f"MG={k}, RING 16, HIGH HETERO",
+# )
+
+
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 n=16
 A,B = ring1(n=n)
-k = 1
+k = 4
 A = np.linalg.matrix_power(A, k)
 show_row(A)
 print(A.shape)
 
-lr_list = [6e-3, 7e-3, 5e-3, 4e-3, 3e-3, 1e-2]
+train_high_hetero(
+    algorithm="PullDiag_GT",
+    lr=2e-3,
+    A=A,
+    B=B,# 实际没用用到
+    dataset_name="MNIST",
+    batch_size=128,
+    num_epochs=400,
+    remark=f"MG={k}, RING 16, HIGH HETERO",
+)
 
-for lr in lr_list:
-    train_high_hetero(
-        algorithm="PullDiag_GT",
-        lr=lr,
-        A=A,
-        B=B,# 实际没用用到
-        dataset_name="MNIST",
-        batch_size=128,
-        num_epochs=300,
-        remark=f"MG={k}, RING 16, HIGH HETERO",
-    )
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+n=16
+A,B = ring1(n=n)
+k = 6
+A = np.linalg.matrix_power(A, k)
+show_row(A)
+print(A.shape)
+
+train_high_hetero(
+    algorithm="PullDiag_GT",
+    lr=2e-3,
+    A=A,
+    B=B,# 实际没用用到
+    dataset_name="MNIST",
+    batch_size=128,
+    num_epochs=400,
+    remark=f"MG={k}, RING 16, HIGH HETERO",
+)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+n=16
+A,B = ring1(n=n)
+k = 8
+A = np.linalg.matrix_power(A, k)
+show_row(A)
+print(A.shape)
+
+train_high_hetero(
+    algorithm="PullDiag_GT",
+    lr=2e-3,
+    A=A,
+    B=B,# 实际没用用到
+    dataset_name="MNIST",
+    batch_size=128,
+    num_epochs=400,
+    remark=f"MG={k}, RING 16, HIGH HETERO",
+)
